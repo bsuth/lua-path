88e8b87cfc33a754dad16e1a886f6e2c5d280d3f
bsuth <bsuth701@gmail.com>
2023-06-23 14:56:11 +0200

    format compiled code

--- path.lua
+++ path.lua
@@ -1,165 +1,38 @@
 local _MODULE = {}
-local separator
-=
-package
-.config
-:sub(
-1
-,
-1
-)
+local separator = package.config:sub(1, 1)
 _MODULE.separator = separator
-local
-function
-dedup
-(s)
-return
-s
-:gsub(
-separator
-..
-(
-'+'
-)
-,
-separator
-)
+local function dedup(s)
+	return s:gsub(separator .. "+", separator)
 end
 _MODULE.dedup = dedup
-local
-function
-join
-(...)
-return
-dedup(
-table
-.concat(
-{
-...
-,
-}
-,
-separator
-)
-)
+local function join(...)
+	return dedup(table.concat({
+		...,
+	}, separator))
 end
 _MODULE.join = join
-local
-function
-trim
-(...)
-return
-join(
-...
-)
-:gsub(
-(
-"^"
-..
-tostring(
-separator
-)
-..
-"+"
-)
-,
-(
-''
-)
-)
-:gsub(
-(
-tostring(
-separator
-)
-..
-"+$"
-)
-,
-(
-''
-)
-)
+local function trim(...)
+	return join(...):gsub(("^" .. tostring(separator) .. "+"), ""):gsub((tostring(separator) .. "+$"), "")
 end
 _MODULE.trim = trim
-local
-function
-pad
-(...)
-return
-separator
-..
-trim(
-...
-)
-..
-separator
+local function pad(...)
+	return separator .. trim(...) .. separator
 end
 _MODULE.pad = pad
-local
-function
-lead
-(...)
-return
-separator
-..
-trim(
-...
-)
+local function lead(...)
+	return separator .. trim(...)
 end
 _MODULE.lead = lead
-local
-function
-trail
-(...)
-return
-trim(
-...
-)
-..
-separator
+local function trail(...)
+	return trim(...) .. separator
 end
 _MODULE.trail = trail
-local
-function
-basename
-(s)
-return
-s
-:match(
-(
-"[^"
-..
-tostring(
-separator
-)
-..
-"]*$"
-)
-)
+local function basename(s)
+	return s:match(("[^" .. tostring(separator) .. "]*$"))
 end
 _MODULE.basename = basename
-local
-function
-dirname
-(s)
-return
-s
-:gsub(
-(
-"[^"
-..
-tostring(
-separator
-)
-..
-"]*$"
-)
-,
-(
-''
-)
-)
+local function dirname(s)
+	return s:gsub(("[^" .. tostring(separator) .. "]*$"), "")
 end
 _MODULE.dirname = dirname
 return _MODULE
